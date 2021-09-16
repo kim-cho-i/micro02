af1e9c7174431a97e5c82017c64c402ba7e2af7d
kim-cho-i <90668331+kim-cho-i@users.noreply.github.com>
2021-09-16 12:47:09 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,29 +1,17 @@
-let 거리 = 0
 input.onButtonPressed(Button.A, function () {
-    music.playTone(294, music.beat(BeatFraction.Whole))
-    music.playTone(330, music.beat(BeatFraction.Whole))
-    music.playTone(392, music.beat(BeatFraction.Half))
-    music.playTone(392, music.beat(BeatFraction.Half))
-    music.playTone(392, music.beat(BeatFraction.Half))
-    music.playTone(392, music.beat(BeatFraction.Half))
-    music.playTone(392, music.beat(BeatFraction.Half))
-    music.playTone(294, music.beat(BeatFraction.Whole))
-    music.playTone(330, music.beat(BeatFraction.Whole))
-    music.playTone(392, music.beat(BeatFraction.Half))
+	
+})
+basic.forever(function () {
+    if (input.temperature() >= 35) {
+        pins.digitalWritePin(DigitalPin.P8, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P8, 0)
+    }
+    basic.showNumber(input.temperature())
 })
 basic.forever(function () {
 	
 })
 basic.forever(function () {
 	
-})
-basic.forever(function () {
-	
-})
-basic.forever(function () {
-    pins.digitalWritePin(DigitalPin.P1, 1)
-    basic.pause(100)
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    거리 = pins.pulseIn(DigitalPin.P0, PulseValue.High) / 40
-    basic.showNumber(거리)
 })
